9fd6491a5a4bb756b90f6ebb1da2846df2831e63
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2015-11-23 01:34:06 -0200

    fixed openshift url server

--- server/config.js
+++ server/config.js
@@ -7,7 +7,7 @@ var autoconfig = function (config_overrides){
   , MOBILE_ENABLED: process.env.MOBILE_ENABLED || false
   , WINNER_COUNT: process.env.WINNER_COUNT || 10
   , NAMESPACE  : process.env.NAMESPACE || process.env.OPENSHIFT_BUILD_NAMESPACE || 'hexboard'
-  , OPENSHIFT_SERVER: process.env.KUBERNETES_SERVICE_HOST || process.env.OPENSHIFT_SERVER || 'localhost:8443'
+  , OPENSHIFT_SERVER: 'master.devops.org:8443'
   , BASIC_AUTH_USER: process.env.BASIC_AUTH_USER || ''
   , BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD || ''
   , HEXBOARD_SIZE: process.env.HEXBOARD_SIZE || 'nano' // nano, micro, tiny, xsmall, small, medium, large, xlarge
